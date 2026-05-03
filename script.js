776a94556ce4d8871c3297a8a7a45f882eb7df1b
Timothy Tesluk <timothy.tesluk@gmail.com>
2026-05-03 12:13:06 +0800

    Add mouse events for utility buttons

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const c0_buttons = document.querySelectorAll(".col0");
 const c1_buttons = document.querySelectorAll(".col1");
 const r0_buttons = document.querySelectorAll(".row0");
 const r1_buttons = document.querySelectorAll(".row1");
+const util_buttons = document.querySelectorAll(".util-button");
 const htmlDOM = document.querySelector('html');
 
 const groups = [c0_buttons,c1_buttons,r0_buttons,r1_buttons];
@@ -60,5 +61,26 @@ for (const button of r0_buttons) {
 for (const button of r1_buttons) {
     button.addEventListener("mousedown", () => {
         Sqr.turn("D");
+    });
+}
+
+for (const util_button of util_buttons) {
+    util_button.addEventListener("mouseenter", () => util_button.classList.toggle("util-hover"));
+    util_button.addEventListener("mouseleave", () => util_button.classList.toggle("util-hover"));
+    util_button.addEventListener("mousedown", () => util_button.classList.toggle("util-press"));
+    util_button.addEventListener("mouseup", () => util_button.classList.toggle("util-press"));
+}
+
+// Failsafe if mouse leaves the element during press
+for (const util_button of util_buttons) {
+    util_button.addEventListener("mouseenter", () => {
+        util_button.addEventListener("mouseleave", () => {
+            if (util_button.classList.contains("util-press")) {
+                util_button.classList.toggle("util-press");
+            }
+            if (util_button.classList.contains("util-hover")) {
+                util_button.classList.toggle("util-hover");
+            }
+        }, {once: true});
     });
 }
